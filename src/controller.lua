7bb6ca8f2f4f162e9d7d2a5fbd9dc7176f200ae4
Martin Dahl <martin.dahl@gmail.com>
2024-02-22 13:21:42 +0100

    agv example

---
+++ src/controller.lua
@@ -0,0 +1,204 @@
+-- some random definitions we need
+function table_invert(t)
+   s={}
+   for k,v in pairs(t) do
+     s[v]=k
+   end
+   return s
+end
+
+-- redefines some agv positions from ARIAC to axelline
+-- (just to avoid rewriting control logic)
+location_to_name = {
+   [0] = 'pou',      -- conveyor
+   [1] = 'waiting',  -- station 1
+   [2] = 'wp18',     -- station 2
+   [3] = 'kitting',  -- depot
+   [99] = 'unknown', -- moving
+}
+name_to_location = table_invert(location_to_name)
+
+-- set up SP communication via redis.
+sp.add_input { lua_name = 'agv1.state', redis_key = 'agv1/state' }
+sp.add_input { lua_name = 'agv2.state', redis_key = 'agv2/state' }
+sp.add_input { lua_name = 'agv3.state', redis_key = 'agv3/state' }
+
+sp.add_output { lua_name = 'agv1.goal', redis_key = 'agv1/goal', ttl = 20 }
+sp.add_output { lua_name = 'agv2.goal', redis_key = 'agv2/goal', ttl = 20 }
+sp.add_output { lua_name = 'agv3.goal', redis_key = 'agv3/goal', ttl = 20 }
+
+-- setup initial states if needed
+agv1 = {
+   position = "waiting",
+   has_kit = true,
+}
+agv2 = {
+   position = "pou",
+   has_kit = true,
+}
+agv3 = {
+   position = "kitting",
+   has_kit = false,
+}
+roles = {
+   waiting = "agv1",
+   pou = "agv2",
+   kitting = "agv3",
+}
+
+-- "pointers"
+function kitting()
+   return _G[roles.kitting]
+end
+
+function waiting()
+   return _G[roles.waiting]
+end
+
+function pou()
+   return _G[roles.pou]
+end
+
+-- For demonstration operations just sleep a bit before finishing the
+-- ack operations.
+dummy_time = 5000
+
+function swap_roles()
+   roles = {
+      kitting = roles.pou,
+      pou = roles.waiting,
+      waiting = roles.kitting
+   }
+end
+
+function axelline_coordinator()
+   if not kitting().operation and kitting().position ~= "kitting" then
+      local op_name = roles.kitting .. "_goto_kitting"
+      sp.add_operation {
+         name = op_name,
+
+         -- guard includes waitings position.
+         start_guard = function (state) return waiting().position == "wp18" end,
+         start_action = function (state)
+            state.started_at = sp.now
+            kitting().goal = name_to_location['kitting']
+            kitting().operation = op_name
+         end,
+
+         -- pretend to finish after some time
+         finish_guard = function (state)
+            return location_to_name[kitting().state.location] == 'kitting'
+         end,
+         finish_action = function (state)
+            kitting().operation = nil
+            kitting().position = "kitting"
+            sp.remove_operation(op_name)
+         end,
+      }
+   end
+
+   if not kitting().operation and kitting().position == "kitting"
+      and not kitting().has_kit then
+      local op_name = roles.kitting .. "_kitting_ack"
+      sp.add_operation {
+         name = op_name,
+
+         start_guard = function (state) return true end,
+         start_action = function (state)
+            state.started_at = sp.now
+            kitting().operation = op_name
+         end,
+
+         finish_guard = function (state)
+            return sp.now > state.started_at + dummy_time
+         end,
+         finish_action = function (state)
+            kitting().operation = nil
+            kitting().has_kit = true
+            sp.remove_operation(op_name)
+         end,
+      }
+   end
+
+   if not waiting().operation
+      and waiting().position ~= "wp18"
+      and waiting().position ~= "waiting" then
+      local op_name = roles.waiting .. "_goto_wp18"
+      sp.add_operation {
+         name = op_name,
+
+         start_guard = function (state) return true end,
+         start_action = function (state)
+            state.started_at = sp.now
+            waiting().goal = name_to_location['wp18']
+            waiting().operation = op_name
+         end,
+
+         finish_guard = function (state)
+            return location_to_name[waiting().state.location] == 'wp18'
+         end,
+         finish_action = function (state)
+            waiting().operation = nil
+            waiting().position = "wp18"
+            sp.remove_operation(op_name)
+         end,
+      }
+   end
+
+   if not waiting().operation
+      and waiting().position == "wp18" then
+      local op_name = roles.waiting .. "_goto_waiting"
+      sp.add_operation {
+         name = op_name,
+
+         -- guard includes kitting position. check
+         -- wp17 etc... for simplicity here we
+         -- check that kitting has moved all the
+         -- way to the kitting wp
+         start_guard = function (state) return kitting().position == "kitting" end,
+         start_action = function (state)
+            state.started_at = sp.now
+            waiting().goal = name_to_location['waiting']
+            waiting().operation = op_name
+         end,
+
+         finish_guard = function (state)
+            return location_to_name[waiting().state.location] == 'waiting'
+         end,
+         finish_action = function (state)
+            waiting().operation = nil
+            waiting().position = "waiting"
+            sp.remove_operation(op_name)
+         end,
+      }
+   end
+
+   if not pou().operation then
+      local op_name = roles.pou .. "_pou_ack"
+      sp.add_operation {
+         name = op_name,
+
+         start_guard = function (state) return true end,
+         start_action = function (state)
+            state.started_at = sp.now
+            pou().operation = op_name
+         end,
+
+         finish_guard = function (state)
+            return waiting().position == "waiting" and kitting().has_kit and
+               sp.now > state.started_at + dummy_time
+         end,
+         finish_action = function (state)
+            pou().operation = nil
+            pou().has_kit = false -- remove the kit.
+            sp.remove_operation(op_name)
+
+            -- when we finish we also want to swap the roles.
+            swap_roles()
+         end,
+      }
+   end
+end
+
+
+sp.add_function("axelline_coordinator", axelline_coordinator)
--- src/main.rs
+++ src/main.rs
@@ -1,142 +1,91 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
 use mlua::{chunk, Lua, Result, Value, Error, LuaSerdeExt, Function, Table};
-use serde::{Deserialize, Serialize};
+use redis::Commands;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Config {
-    parameter1: String,
-    parameter2: f64,
+pub struct InputMapping {
+    key: String,
+    var: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Robot {
-    active: bool,
-    state: i32,
-    config: Config,
+pub struct OutputMapping {
+    key: String,
+    var: String,
+    ttl: usize,
 }
 
 fn main() -> Result<()> {
     let lua = Lua::new();
     add_dump_function(&lua)?;
-    let globals = lua.globals();
+    add_sp_functions(&lua)?;
 
-    // Write robot1 to lua land
-    let robot1 = Robot {
-        active: true,
-        state: 1,
-        config: Config {
-            parameter1: "something".into(),
-            parameter2: 5.0,
+    let r = redis::Client::open("redis://10.7.0.35/").unwrap();
+    let mut rc = r.get_connection().unwrap();
+    // Fetch the code and run it
+    // let code: String = rc.get("sp/code").map_err(Error::external)?;
+    // lua.load(&code).exec()?;
+    load_and_run(&lua, "src/controller.lua")?;
+
+    // Get input mappings
+    let mut inputs = vec![];
+    if let Ok(v) = lua.load("sp.inputs").eval::<Table>() {
+        for pair in v.pairs::<String, String>() {
+            let (key, value) = pair?;
+            inputs.push(InputMapping {
+                key: value,
+                var: key,
+            });
         }
-    };
-
-    globals.set("robot1", lua.to_value(&robot1)?)?;
-
-    // robot2 is created in lua land
-    lua.load(r#"robot2 = {active = true, state = 1, config = { parameter1 = "other", parameter2 = 10.0 }}"#)
-        .eval()?;
-
-    let robot2: Robot = lua.from_value(globals.get("robot2")?).map_err(Error::external)?;
-    println!("robot2: {:?}", robot2);
-
-    // Run some transitions.
-
-    // eval a guard
-    let guard: bool = lua.load("robot1.active or robot2.active").eval()?;
-    println!("result: {guard}");
-
-    // eval an action
-    lua.load("robot1.active = not robot1.active").eval()?;
-
-    // lua transition helpers
-    lua.load(chunk! {
-        function add_transition(t)
-            table.insert(transitions, t)
-        end
-        function run_transitions()
-            for i, t in ipairs(transitions) do
-              if t.guard() then
-//                  print("taking " .. t.name)
-                  t.action()
-              end
-            end
-        end
-        transitions = {}
-        scratch = {counter = 0}
-    }).exec()?;
-
-    // Make a sp model with two transitions
-    lua.load(chunk! {
-        add_transition{name = "t1",
-                       guard = function () return not robot1.active end,
-                       action = function () robot1.active = true; scratch.counter += 1 return end
-        }
-        add_transition{name = "t2",
-                       guard = function () return robot1.active end,
-                       action = function () robot1.active = false; scratch.counter += 1 return end
-        }
-    }).exec()?;
-
-    // Run a million cycles in lua land
-    let t = now();
-    lua.load(chunk! {
-        for i=1,1000000 do
-            run_transitions()
-        end
-    }).exec()?;
-    let passed = now() - t;
-    println!("Lua evaluation took: {}ms", passed.as_millis());
-
-    // Or we can run the transitions from rust land instead.
-    // (but predicates and actions are still evaluated in lua land)
-    // To see how much overhead we have.
-    pub struct Transition<'a> {
-        pub name: String,
-        pub guard: Function<'a>,
-        pub action: Function<'a>,
     }
 
-    // load transitions from lua land to rust land
-    let lua_transitions: Table = globals.get("transitions")?;
-    let mut transitions = vec![];
-    lua_transitions.for_each::<Value, Table>(|_,v| {
-        let name: String = v.get("name")?;
-        let guard: Function = v.get("guard")?;
-        let action: Function = v.get("action")?;
-        transitions.push(Transition {
-            name,
-            guard,
-            action,
-        });
-        Ok(())
-    })?;
+    // Get output mappings
+    let mut outputs = vec![];
+    if let Ok(v) = lua.load("sp.outputs").eval::<Table>() {
+        for pair in v.pairs::<String, Table>() {
+            let (key, value) = pair?;
 
-    let t = now();
-    for _ in 0..1000000 {
-        for t in &transitions {
-            let r: bool = t.guard.call(())?;
-            if r {
-//                println!("Took transition: {}", v.get::<_, String>("name")?);
-                t.action.call(())?;
+            outputs.push(OutputMapping {
+                key: value.get("redis_key")?,
+                var: key,
+                ttl: value.get("ttl")?,
+            });
+        }
+    }
+
+    loop {
+        for i in &inputs {
+            let r: std::result::Result<String, _> = rc.get(&i.key);
+            if let Ok(g) = r {
+                let value = serde_json::from_str::<serde_json::Value>(&g).map_err(Error::external)?;
+                if let Some((parent, field)) = i.var.rsplit_once(".") {
+                    let table = lua.load(parent).eval::<Table>()?;
+                    table.set(field, lua.to_value(&value)?)?;
+                } else {
+                    // no split, assume global
+                    lua.globals().set(i.var.clone(), lua.to_value(&value)?)?;
+                }
             }
         }
-    }
-    let passed = now() - t;
-    println!("Rust evaluation took: {}ms", passed.as_millis());
 
-    // Fetch outputs from lua to rust.
+        // Update with the current time.
+        let now_millis = now().as_millis();
+        lua.load(chunk! {
+            sp.now = $now_millis
+        }).exec()?;
 
-    // "scratch" is an unstructured lua table, can only get as json
-    let scratch: Value = globals.get("scratch")?;
-    println!("scratch is {}", serde_json::to_string(&scratch).map_err(Error::external)?);
+        // Run tick function.
+        let tick: Function = lua.load("sp.tick").eval()?;
+        tick.call::<_, ()>(())?;
 
-    // robot state is a rust struct.
-    let robot1: Robot = lua.from_value(globals.get("robot1")?).map_err(Error::external)?;
-    let robot2: Robot = lua.from_value(globals.get("robot2")?).map_err(Error::external)?;
+        for o in &outputs {
+            if let Ok(v) = lua.load(&o.var).eval::<Value>() {
+                let json = serde_json::to_string_pretty(&v).map_err(Error::external)?;
+                let _r: std::result::Result<(), _> = rc.set_ex(&o.key, json, o.ttl);
+            }
+        }
 
-    println!("robot1: {:?}", robot1);
-    println!("robot2: {:?}", robot2);
-
-    Ok(())
+        std::thread::sleep(std::time::Duration::from_millis(100));
+   }
 }
 
 pub fn add_dump_function(lua: &Lua) -> Result<()> {
@@ -165,6 +114,115 @@ pub fn add_dump_function(lua: &Lua) -> Result<()> {
            end
        "#).exec()
 }
+
+pub fn add_sp_functions(lua: &Lua) -> Result<()> {
+    // Helper function to debug print lua tables.
+    lua.load(
+        r#"
+           sp = {}
+
+           function sp.add_input(i)
+             sp.inputs = sp.inputs or {}
+             sp.inputs[i.lua_name] = i.redis_key
+           end
+
+           function sp.add_output(o)
+             sp.outputs = sp.outputs or {}
+             sp.outputs[o.lua_name] = { redis_key = o.redis_key, ttl = o.ttl or 10 }
+           end
+
+           function sp.tick()
+              local fired, errors = sp.take_transitions()
+              return #fired
+           end
+
+           function sp.take_transitions()
+              local fired = {}
+              local errors = {}
+
+              -- run functions
+              for _, f in pairs(sp.functions or {}) do
+                 f()
+              end
+
+              -- run operations
+              for name, o in pairs(sp.operations or {}) do
+                 if o.state.state == "i" then
+                    local status, result_or_error = pcall(o.start_guard, o.state)
+                    if status and result_or_error then
+                       o.state.state = "e"
+                       o.start_action(o.state)
+                       table.insert(fired, "start_" .. name)
+                       print("Started " .. o.name)
+                    elseif not status then
+                       print("Operation error " .. o.name .. ": " .. result_or_error)
+                       table.insert(errors, "start_" .. name .. ": " .. result_or_error)
+                    end
+                 elseif o.state.state == "e" then
+                    local status, result_or_error = pcall(o.finish_guard, o.state)
+                    if status and result_or_error then
+                       o.state.state = "f"
+                       o.finish_action(o.state)
+                       table.insert(fired, "finish_" .. name)
+                       print("Finished " .. o.name)
+                    elseif not status then
+                       print("Operation error " .. o.name .. ": " .. result_or_error)
+                       table.insert(errors, "finish_" .. name .. ": " .. result_or_error)
+                    end
+                 elseif o.state.state == "f" then
+                    local status, result_or_error = pcall(o.reset_guard, o.state)
+                    if status and result_or_error then
+                       o.state.state = "i"
+                       o.reset_action(o.state)
+                       table.insert(fired, "reset_" .. name)
+                       print("Reset " .. o.name)
+                    elseif not status then
+                       table.insert(errors, "reset_" .. name .. ": " .. result_or_error)
+                    end
+                 end
+              end
+
+              return fired, errors
+           end
+
+           function sp.add_function(name, f)
+              sp.functions = sp.functions or {}
+              sp.functions[name] = f
+           end
+
+           function sp.add_operation(o)
+              local o = o or {}
+              o.state = { state = "i" }
+              o.start_guard = o.start_guard or function (state) return false end
+              o.start_action = o.start_action or function (state) end
+
+              o.finish_guard = o.finish_guard or function (state) return true end
+              o.finish_action = o.finish_action or function (state) end
+
+              o.reset_guard = o.reset_guard or function (state) return false end
+              o.reset_action = o.reset_action or function (state) end
+
+              sp.operations = sp.operations or {}
+              sp.operations[o.name] = o
+           end
+
+           function sp.remove_operation(name)
+              if sp.operations and sp.operations[name] ~= nil then
+                 sp.operations[name] = nil
+              end
+           end
+
+       "#).exec()
+}
+
+
+pub fn load_and_run(lua: &Lua, filename: &str) -> Result<()> {
+    let mut input: BufReader<File> = BufReader::new(File::open(filename)?);
+    let mut s = String::new();
+    input.read_to_string(&mut s)?;
+    lua.load(s).exec()
+}
+
 
 pub fn now() -> std::time::Duration {
     std::time::SystemTime::now()
